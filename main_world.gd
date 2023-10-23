dd296e3c848ebb51f771540b39fdb76689fd4df3
nikob16a1 <148738514+nikob16a1@users.noreply.github.com>
2023-10-23 11:19:55 +0700

    Add files via upload

---
+++ main_world.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var c
+
+func _ready():
+	RenderingServer.set_default_clear_color(Color.SKY_BLUE)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
